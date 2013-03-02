67547120a91ac1141a307be9a6f448bcfae4a603
nobuti <buti@nobuti.com>
2013-03-02 19:52:41 +0100

    Some typo fixes

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -15,11 +15,23 @@ describe "Portfolio" do
   end
 
   it "should have github link" do
-    last_response.body.should match(/<a class="link" href="https:\/\/github.com\/nobuti" titile="github">https:\/\/github.com\/nobuti<\/a>/)
+    last_response.body.should match(/<a class="link" href="https:\/\/github.com\/nobuti" title="github">https:\/\/github.com\/nobuti<\/a>/)
   end
 
   it "should have contact link" do
-    last_response.body.should match(/<a class="link" href="mailto: buti@nobuti.com" title="hire me">Let's talk<\/a>/)
+    last_response.body.should match(/<a class="link" href="mailto:buti@nobuti.com" title="hire me">Let's talk<\/a>/)
+  end
+
+end
+
+describe "Resume" do
+  
+  before(:each) do
+    get "/resume"
+  end    
+
+  it "should respond to GET /resume" do
+    last_response.should be_ok
   end
 
 end
